cfc337f1e47de3de30e02fb34f0c1738cc2956df
Kamlesh Pandey <kamleshpandey25k@gmail.com>
2026-01-25 00:13:43 +0530

    Add PrintUpperTringle.py

---
+++ PrintUpperTringle.py
@@ -0,0 +1,10 @@
+def upperTringle(nums):
+    for i in range(0,len(nums)):
+        for j in range(0,len(nums[0])):
+            if j>=i :
+                print(nums[i][j],end=" ")
+            else :
+                print(" ",end=" ")
+        print()
+nums=[[1,2,3],[4,5,6],[7,8,9]]
+upperTringle(nums)
